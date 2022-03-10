6da525eeb8af02f14a1e79ade3615e512f0d48db
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-03-10 23:19:42 +0100

    fixed Search view

--- app/src/main/java/com/example/studygroup/main/MainActivity.kt
+++ app/src/main/java/com/example/studygroup/main/MainActivity.kt
@@ -4,43 +4,40 @@ package com.example.studygroup.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
+//import android.view.Menu
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 
 //import com.example.studygroup.data.SubjectDataHandler
-import com.example.studygroup.R
+//import com.example.studygroup.R
 import com.example.studygroup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(){
     private lateinit var binding: ActivityMainBinding
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.options_menu,menu)
-        return true
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val simple =  findViewById<SearchView>(R.id.search)
-        simple.setOnQueryTextListener( object : SearchView.OnQueryTextListener{
-            override fun onQueryTextChange(newText: String?): Boolean {
-                Toast.makeText(this@MainActivity,"Changed",Toast.LENGTH_SHORT).show()
-                return false
+         binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+             android.widget.SearchView.OnQueryTextListener {
+             override fun onQueryTextSubmit(query: String?): Boolean {
+                 Toast.makeText(this@MainActivity,"Changed",Toast.LENGTH_SHORT).show()
+                 return true
+             }
 
-            }
-
-            override fun onQueryTextSubmit(p0: String?): Boolean {
-
-                Toast.makeText(this@MainActivity,"Submitted",Toast.LENGTH_SHORT).show()
-                return true
-            }
-        })
+             override fun onQueryTextChange(newText: String?): Boolean {
+                 Toast.makeText(this@MainActivity,"Submitted",Toast.LENGTH_SHORT).show()
+                 return true
+             }
+         })
 
 
 
     }
 
 }
+
+
