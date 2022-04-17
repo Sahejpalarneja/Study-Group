dbb67194df1c60c098e0e337c44e0df68e716b24
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-04-17 22:06:11 +0200

    Cleared redundant comments

--- app/src/main/java/com/example/studygroup/data/SubjectDataHandler.kt
+++ app/src/main/java/com/example/studygroup/data/SubjectDataHandler.kt
@@ -16,23 +16,16 @@ import com.google.firebase.ktx.Firebase
 @Suppress("UNCHECKED_CAST")
 class SubjectDataHandler {
 
-    //val database = Firebase.database("https://study-group-c445e-default-rtdb.europe-west1.firebasedatabase.app/")
-
     companion object {
         var  Subjects = ArrayList<Subjects>()
-        val SuggestionList =  ArrayList<String>()
         val database = Firebase.database("https://study-group-c445e-default-rtdb.europe-west1.firebasedatabase.app/")
         val ref = database.getReference("Subjects")
-        //val cursorAdapter = SimpleCursorAdapter(MainActivity,MainActivity.getSearchView())
+
         fun InitializeSubjects(){
 
             ref.addValueEventListener(object:ValueEventListener{
                 override fun onDataChange(snapshot: DataSnapshot){
-                    // This method is called once with the initial value and again
-                    // whenever data at this location is updated.
                     val subjects = snapshot.children.iterator()
-
-
                     while (subjects.hasNext())
                     {
                         var subject= listOf(subjects.next().children)[0]
@@ -46,20 +39,8 @@ class SubjectDataHandler {
                             var students = neptun.child("students").value
                             Subjects.add(Subjects(code as String,name as String, professors as ArrayList<String>, students as ArrayList<String>))
                         }
-
-                        //Log.w(TAG, "Failed to read value.")
-                        //System.out.println(
-                        //var name= subjects.child("NEPTUN")
-                        //var professors = subject.get("professors")
-                        //var name = subject.child("name").value.toString()
-                        //var students = subject.child("students").value
-
-                        //))test
-                        //SuggestionList.add(name)
                     }
-
                 }
-
                 override fun onCancelled(error: DatabaseError) {
                     Log.w(TAG, "Failed to read value.", error.toException())
                 }
