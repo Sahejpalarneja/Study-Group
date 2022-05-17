18e5a2384482ea0d8791087ece3d475399ca2362
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-05-17 21:50:47 +0200

    Created Message Activity and layout

---
+++ app/src/main/java/com/example/studygroup/Chat/ChatActivity.kt
@@ -0,0 +1,11 @@
+package com.example.studygroup.Chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
