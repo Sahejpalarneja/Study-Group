dee84c1ec759be2cbc3a09390cd1b2c94ce56aec
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-05-18 11:44:39 +0200

    Created Message Adapter

---
+++ app/src/main/java/com/example/studygroup/adapters/MessageAdapter.kt
@@ -0,0 +1,80 @@
+package com.example.studygroup.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+
+
+import android.view.ViewGroup
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.studygroup.ButtonActivities.SubjectUserUtils
+import com.example.studygroup.data.Message
+
+import com.example.studygroup.databinding.ReceiveMessageBinding
+import com.example.studygroup.databinding.SentLayoutBinding
+
+class MessageAdapter(val context: Context,messageList :ArrayList<Message>):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    val ITEM_RECEIVE = 1
+    val ITEM_SENT = 2
+    private var messageList = mutableListOf<Message>()
+    init {
+        this.messageList.addAll(messageList)
+    }
+    inner class SentViewHolder(val binding:SentLayoutBinding):RecyclerView.ViewHolder(binding.root){
+        val sentMessage = binding.tvSentMessage
+    }
+    inner class ReceiveViewHolder(val binding:ReceiveMessageBinding):RecyclerView.ViewHolder(binding.root){
+        val recievedMessage = binding.tvRecievedMessage
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+      if(viewType ==1){
+          return ReceiveViewHolder(
+              ReceiveMessageBinding.inflate(
+                  LayoutInflater.from(parent.context),
+                  parent,
+                  false
+              ))
+      }
+        else{
+          return SentViewHolder(
+              SentLayoutBinding.inflate(
+                  LayoutInflater.from(parent.context),
+                  parent,
+                  false
+              ))
+      }
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        var currentMessage:Message = messageList[position]
+       if(holder.javaClass == SentViewHolder::class.java){
+           //do the stufff
+
+           val viewHolder = holder as SentViewHolder
+
+           holder.binding.tvSentMessage.text = currentMessage.message
+       }
+        else{
+           val viewHolder = holder as ReceiveViewHolder
+           holder.binding.tvRecievedMessage.text =currentMessage.message
+            //do stuff for receive
+       }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+       val currentMessage = messageList[position]
+        if(SubjectUserUtils.getUser().UserID.equals(currentMessage.senderId))
+        {
+            return  ITEM_SENT
+        }
+        else{
+            return ITEM_RECEIVE
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return messageList.size
+    }
+}
