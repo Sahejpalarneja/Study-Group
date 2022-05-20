62ff14699b7c120a6dd7e9866d6d020faf89f108
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-05-20 10:50:39 +0200

    Added names to the messages

--- app/src/main/java/com/example/studygroup/data/Message.kt
+++ app/src/main/java/com/example/studygroup/data/Message.kt
@@ -2,12 +2,12 @@ package com.example.studygroup.data
 
 class Message {
     var message:String? = null
-    var senderId :String? = null
+    var username :String? = null
 
     constructor(){}
     constructor(_message:String?,_senderId:String?)
     {
         this.message = _message
-        this.senderId = _senderId
+        this.username = _senderId
     }
 }
