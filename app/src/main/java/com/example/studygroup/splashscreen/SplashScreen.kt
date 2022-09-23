b169f42bb5985fa4c43c462a092d9684f30753ac
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-09-23 22:56:02 +0200

    Removed Imports and Subject init

--- app/src/main/java/com/example/studygroup/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/studygroup/splashscreen/SplashScreen.kt
@@ -1,22 +1,28 @@
 package com.example.studygroup.splashscreen
 
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.studygroup.Handlers.SubjectDataHandler
 import com.example.studygroup.databinding.ActivitySplashScreenBinding
 import com.example.studygroup.login.LoginActivity
+import com.example.studygroup.models.Subject
+import com.example.studygroup.network.RetrofitClient
+import com.example.studygroup.utils.SubjectUserUtils
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
     private lateinit var binding: ActivitySplashScreenBinding
-
-    override fun onStart() {
-        super.onStart()
-        SubjectDataHandler.InitializeSubjects()
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
@@ -33,5 +39,7 @@ class SplashScreen : AppCompatActivity() {
             finish()
         },1000
         )
+
     }
+
 }
