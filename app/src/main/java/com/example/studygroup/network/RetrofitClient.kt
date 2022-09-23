b94c1f48e18d6d930aa87d91431125e5b30dd8f7
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-09-23 21:17:31 +0200

    Create endpoints for login post request

---
+++ app/src/main/java/com/example/studygroup/network/RetrofitClient.kt
@@ -0,0 +1,20 @@
+package com.example.studygroup.network
+
+import com.example.studygroup.models.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
+import java.util.*
+import kotlin.collections.ArrayList
+
+public interface RetrofitClient {
+
+        @GET("messages/mobileid")
+        @Headers("Authorization:Token 9ae0eb8a602eb76a47c8c5b553b20ee03adb9631")
+        fun get_messages():Call<ArrayList<Message>>
+
+        @POST("login")
+        fun login(@Body login_user : LoginUser):Call<Token>
+
+}
