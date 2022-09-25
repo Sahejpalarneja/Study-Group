993bab9514ac9290ba1df85fcdff1795f19cb96a
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-09-25 11:32:54 +0200

    Added join class endpoint

--- app/src/main/java/com/example/studygroup/network/RetrofitClient.kt
+++ app/src/main/java/com/example/studygroup/network/RetrofitClient.kt
@@ -8,20 +8,33 @@ import retrofit2.http.*
 import java.util.*
 import kotlin.collections.ArrayList
 
-public interface RetrofitClient {
+interface RetrofitClient {
 
         @GET("messages/mobileid")
-        fun get_messages():Call<ArrayList<Message>>
+        fun get_messages(): Call<ArrayList<Message>>
 
         @POST("login")
-        fun login(@Body login_user : LoginUser):Call<Token>
+        fun login(@Body login_user: LoginUser): Call<Token>
 
         @GET("subjects")
-        fun getSubjects():Call<ArrayList<Subject>>
+        fun getSubjects(): Call<ArrayList<Subject>>
 
         @GET("get_id")
-        fun getUserID(@Header("Authorization")token:String,@Query("username")username:String):Call<Id>
+        fun getUserID(
+                @Header("Authorization") token: String,
+                @Query("username") username: String
+        ): Call<Id>
 
         @GET("get_user_subjects")
-        fun getUserSubjects(@Header("Authorization")token:String,@Query("id")id:Int):Call<ArrayList<Subject>>
+        fun getUserSubjects(
+                @Header("Authorization") token: String,
+                @Query("id") id: Int
+        ): Call<ArrayList<Subject>>
+
+        @POST("join_class")
+        fun postJoinClass(
+                @Header("Authorizatoin") token: String,
+                @Query("neptun") neptun: String,
+                @Query("id") id: Int
+        ): Call<Response>
 }
